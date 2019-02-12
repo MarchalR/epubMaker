ed3558f2410072c3f0b8ab083571b313c0b92954
Marchal Rémi <remi.marchal6@gmail.com>
2019-02-12 13:09:19 +0100

    Add files via upload
    tentative include quazip

---
+++ Epub_Maker/epubMaker/mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    QString sourceDir;
+    QString destinationDir;
+    QString gabaritDir;
+    QString massDir;
+
+private slots:
+
+    void on_goButton_clicked();
+
+    void on_selectImageFolder_clicked();
+
+    void on_selectDestinationFolder_clicked();
+
+    void on_selectGabarit_clicked();
+
+    void on_selectMassFolder_clicked();
+
+    void on_lunchMassConvert_clicked();
+
+    void on_selectGabaritMass_clicked();
+
+    void on_testButton_clicked();
+
+
+
+private:
+    Ui::MainWindow *ui;    
+    int findFiles();
+    bool createEpubDir(QString sourceDir, QString destinationDir, bool overWriteDirectory);
+    void createPages(int nbPages);
+    void createOpf(int nbPages);
+    void createToc(int nbPages, QString name);
+    void folderZip(QString sourceDir, QString destinationDir);
+    QString selectFolder();
+    //void recurseAddDir(QDir d, QStringList & list);
+};
+
+#endif // MAINWINDOW_H
